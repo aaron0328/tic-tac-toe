bfeac45733df3dbce75dbe425deb117713db2d2e
hei0328 <aaron0328@hotmail.com>
2020-05-08 09:59:19 +0800

    Updates

---
+++ precache-manifest.e3e65bd154c11b454f22461c4566592b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976c5e0ffd6c0f3ea3a2d38cc3d88f6d",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3dc2796439efa4383f89",
+    "url": "/tic-tac-toe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "dccf10a0366b101a0416",
+    "url": "/tic-tac-toe/static/js/2.638b2c12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.638b2c12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dc2796439efa4383f89",
+    "url": "/tic-tac-toe/static/js/main.9467beac.chunk.js"
+  },
+  {
+    "revision": "67093bcd19aa82d2fa67",
+    "url": "/tic-tac-toe/static/js/runtime-main.29e8e2be.js"
+  }
+]);
